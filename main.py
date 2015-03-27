c102169d263162fd6e438a25c5152ca0ebae6bbd
Sittzon <stigson87@gmail.com>
2015-03-27 12:14:11 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+__author__ = 'Sittzon'
+#To change this template use Tools | Templates.
+#
+
+class main:
+    def __init__(self):
+        self.stuff = ""
+    
+    def start(self):
+        out = "starting emulator..."
+        print(out)
+        
+x = main()
+x.start()
